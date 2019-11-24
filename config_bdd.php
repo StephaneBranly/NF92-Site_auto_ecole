e9e852a710f1acde7b8d77755af473ce095fc21e
Stephane_Branly <51955833+StephaneBranly@users.noreply.github.com>
2019-11-24 21:54:26 +0100

    Update config_bdd.php

--- config_bdd.php
+++ config_bdd.php
@@ -1,7 +1,7 @@
 <?php
-	// Variables de connection à la BDD
+	// Variables de connection à la BDD ; A modifier suivant sa BDD
 	$dbhost="localhost";
 	$dbuser="root";
 	$dbpass="mysql";
-	$dbname="nf92a072";
+	$dbname="nf92****";
 ?>
